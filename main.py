4a0a14908d3ac392bb1787c28035cdcfc4f09374
randomu3 <demiz@mail.ru>
2023-08-23 12:02:36 +0000

    refactoring of code

--- main.py
+++ main.py
@@ -8,31 +8,27 @@ from config import CHAT_ID, OPENAI_API_KEY, API_TOKEN, WEBHOOK_PATH, WEBHOOK_URL
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
+# Инициализация OpenAI и Aiogram
 openai.api_key = OPENAI_API_KEY
-
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
-last_user_message = None
-user_role = None
-waiting_for_role = False
-
 # Словарь с ролями
-roles_dict = {
+ROLES_DICT = {
     "Программист": "Программист, эксперт в Python и AI",
     "Экономист": "Экономист, специализирующийся на макроэкономике",
     "Историк": "Историк, специализирующийся на средних веках",
 }
 
-main_keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
-main_keyboard.add(KeyboardButton("Установить роль"))
-main_keyboard.add(KeyboardButton("Остановить"))
-main_keyboard.add(KeyboardButton("Помощь"))
+# Клавиатура для основных команд
+MAIN_KEYBOARD = ReplyKeyboardMarkup(resize_keyboard=True)
+MAIN_KEYBOARD.add(KeyboardButton("Установить роль"))
+MAIN_KEYBOARD.add(KeyboardButton("Остановить"))
+MAIN_KEYBOARD.add(KeyboardButton("Помощь"))
 
-async def generate_response(prompt_text: str) -> str:
-    global user_role
+# Генерация ответа с помощью OpenAI
+async def generate_response(prompt_text: str, user_role: str) -> str:
     system_message = user_role if user_role else "Ученый который знает все способы заработка и понимает как работать с людьми"
-    
     try:
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo-16k-0613",
@@ -48,56 +44,35 @@ async def generate_response(prompt_text: str) -> str:
         logger.error(f"Error generating response from OpenAI: {e}")
         return "Произошла ошибка при генерации ответа."
 
+# Обработчики сообщений
 @dp.message_handler(commands=['start'])
 async def on_start(message: types.Message):
-    global waiting_for_role
-    await message.answer("Привет! Пожалуйста, укажите роль для бота.", reply_markup=main_keyboard)
-    waiting_for_role = True
+    await message.answer("Привет! Пожалуйста, укажите роль для бота.", reply_markup=MAIN_KEYBOARD)
     logger.info(f"User {message.from_user.id} started the bot.")
 
 @dp.message_handler(lambda message: message.text == "Установить роль")
 async def prompt_role(message: types.Message):
     role_keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
-    for role in roles_dict.keys():
+    for role in ROLES_DICT.keys():
         role_keyboard.add(KeyboardButton(role))
     await message.answer("Выберите роль из предложенных или введите свою.", reply_markup=role_keyboard)
 
-@dp.message_handler(lambda message: waiting_for_role and message.text in roles_dict, content_types=types.ContentType.TEXT)
+@dp.message_handler(lambda message: message.text in ROLES_DICT, content_types=types.ContentType.TEXT)
 async def set_predefined_role(message: types.Message):
-    global user_role, waiting_for_role
-    user_role = roles_dict[message.text]
-    waiting_for_role = False
-    await message.answer(f"Роль установлена как: {user_role}. Теперь вы можете начать диалог!", reply_markup=main_keyboard)
+    global user_role  # Добавьте эту строку
+    user_role = ROLES_DICT[message.text]
+    await message.answer(f"Роль установлена как: {user_role}. Теперь вы можете начать диалог!", reply_markup=MAIN_KEYBOARD)
     logger.info(f"User {message.from_user.id} set role as: {user_role}")
 
-@dp.message_handler(lambda message: waiting_for_role, content_types=types.ContentType.TEXT)
-async def set_custom_role(message: types.Message):
-    global user_role, waiting_for_role
-    user_role = message.text
-    waiting_for_role = False
-    await message.answer(f"Роль установлена как: {user_role}. Теперь вы можете начать диалог!", reply_markup=main_keyboard)
-    logger.info(f"User {message.from_user.id} set role as: {user_role}")
-
-@dp.message_handler(lambda message: message.text == "Остановить")
-async def stop_bot(message: types.Message):
-    await message.answer("Бот остановлен!", reply_markup=types.ReplyKeyboardRemove())
-    logger.info(f"User {message.from_user.id} stopped the bot.")
-
-@dp.message_handler(lambda message: message.text == "Помощь")
-async def help_bot(message: types.Message):
-    help_text = START_TEXT
-    await message.answer(help_text, reply_markup=main_keyboard)
-
-@dp.message_handler(lambda message: not waiting_for_role, content_types=types.ContentType.TEXT)
-async def on_message(message: types.Message):
-    global last_user_message
+@dp.message_handler(content_types=types.ContentType.TEXT)
+async def handle_message(message: types.Message):
     user_message = message.text
-    last_user_message = user_message
     logger.info(f"Received message from user {message.from_user.id}: {user_message}")
 
-    response = await generate_response(user_message)
+    response = await generate_response(user_message, user_role)
     await bot.send_message(CHAT_ID, response, parse_mode=ParseMode.MARKDOWN)
 
+# Обработчики жизненного цикла бота
 async def on_startup(dp):
     webhook_info = await bot.get_webhook_info()
     logger.info(f"Current webhook info: {webhook_info}")
@@ -118,7 +93,7 @@ async def on_shutdown(dp):
     except Exception as e:
         logger.error(f"Error deleting the webhook: {e}")
 
-
+# Запуск бота
 if __name__ == '__main__':
     from aiogram import executor
     executor.start_webhook(
@@ -130,6 +105,3 @@ if __name__ == '__main__':
         host=WEBAPP_HOST,
         port=WEBAPP_PORT,
     )
-# if __name__ == '__main__':
-#     from aiogram import executor
-#     executor.start_polling(dp, skip_updates=True)
