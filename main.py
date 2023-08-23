7f3d6613b2557a28e5dcc3084fc2ab37d078e7d5
randomu3 <demiz@mail.ru>
2023-08-23 03:40:07 +0000

    added logging

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import openai
+import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.types import ParseMode
 from config import CHAT_ID, OPENAI_TOKEN, TG_TOKEN
+
+# Настройка логирования
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
 API_TOKEN = TG_TOKEN
 OPENAI_API_KEY = OPENAI_TOKEN
@@ -23,16 +28,19 @@ async def generate_response(prompt_text: str) -> str:
         model="gpt-3.5-turbo-16k-0613",
         messages=[
             {"role": "system", "content": system_message},
-            {"role": "user", "content": prompt_text} 
+            {"role": "user", "content": prompt_text}
         ]
     )
-    return response.choices[0].message['content'].strip()
+    answer = response.choices[0].message['content'].strip()
+    logger.info(f"Generated response: {answer}")
+    return answer
 
 @dp.message_handler(commands=['start'])
 async def on_start(message: types.Message):
     global waiting_for_role
     await message.answer("Привет! Пожалуйста, укажите роль для бота.")
     waiting_for_role = True
+    logger.info(f"User {message.from_user.id} started the bot.")
 
 @dp.message_handler(lambda message: waiting_for_role, content_types=types.ContentType.TEXT)
 async def set_role(message: types.Message):
@@ -40,16 +48,19 @@ async def set_role(message: types.Message):
     user_role = message.text
     waiting_for_role = False
     await message.answer(f"Роль установлена как: {user_role}. Теперь вы можете начать диалог!")
+    logger.info(f"User {message.from_user.id} set role as: {user_role}")
 
 @dp.message_handler(commands=['stop'])
 async def on_stop(message: types.Message):
     await message.answer("Бот остановлен!")
+    logger.info(f"User {message.from_user.id} stopped the bot.")
 
 @dp.message_handler(lambda message: not waiting_for_role, content_types=types.ContentType.TEXT)
 async def on_message(message: types.Message):
     global last_user_message
     user_message = message.text
     last_user_message = user_message
+    logger.info(f"Received message from user {message.from_user.id}: {user_message}")
 
     response = await generate_response(user_message)
     await bot.send_message(CHAT_ID, response, parse_mode=ParseMode.MARKDOWN)
