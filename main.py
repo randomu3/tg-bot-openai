eb98db6fb40316813abe191450d49a85ce61ea6a
randomu3 <demiz@mail.ru>
2023-08-23 05:30:41 +0000

    stable version with webhook

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import openai
 import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.types import ParseMode, ReplyKeyboardMarkup, KeyboardButton
-from config import CHAT_ID, OPENAI_TOKEN, TG_TOKEN
+from config import CHAT_ID, OPENAI_TOKEN, TG_TOKEN, WEBHOOK_PATH, WEBHOOK_URL, WEBAPP_HOST, WEBAPP_PORT
 
 # Настройка логирования
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -36,16 +36,20 @@ async def generate_response(prompt_text: str) -> str:
     global user_role
     system_message = user_role if user_role else "Ученый который знает все способы заработка и понимает как работать с людьми"
     
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo-16k-0613",
-        messages=[
-            {"role": "system", "content": system_message},
-            {"role": "user", "content": prompt_text}
-        ]
-    )
-    answer = response.choices[0].message['content'].strip()
-    logger.info(f"Generated response: {answer}")
-    return answer
+    try:
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo-16k-0613",
+            messages=[
+                {"role": "system", "content": system_message},
+                {"role": "user", "content": prompt_text}
+            ]
+        )
+        answer = response.choices[0].message['content'].strip()
+        logger.info(f"Generated response: {answer}")
+        return answer
+    except Exception as e:
+        logger.error(f"Error generating response from OpenAI: {e}")
+        return "Произошла ошибка при генерации ответа."
 
 @dp.message_handler(commands=['start'])
 async def on_start(message: types.Message):
@@ -97,6 +101,38 @@ async def on_message(message: types.Message):
     response = await generate_response(user_message)
     await bot.send_message(CHAT_ID, response, parse_mode=ParseMode.MARKDOWN)
 
+async def on_startup(dp):
+    webhook_info = await bot.get_webhook_info()
+    logger.info(f"Current webhook info: {webhook_info}")
+    logger.info("Starting up...")
+    try:
+        await bot.send_message(CHAT_ID, "Bot has been started")
+        await bot.set_webhook(WEBHOOK_URL)
+        logger.info("Webhook set successfully!")
+    except Exception as e:
+        logger.error(f"Error setting up the webhook: {e}")
+
+async def on_shutdown(dp):
+    logger.info("Shutting down...")
+    try:
+        await bot.send_message(CHAT_ID, "Bot has been stopped")
+        await bot.delete_webhook()
+        logger.info("Webhook deleted successfully!")
+    except Exception as e:
+        logger.error(f"Error deleting the webhook: {e}")
+
+
 if __name__ == '__main__':
     from aiogram import executor
-    executor.start_polling(dp, skip_updates=True)
+    executor.start_webhook(
+        dispatcher=dp,
+        webhook_path=WEBHOOK_PATH,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        skip_updates=True,
+        host=WEBAPP_HOST,
+        port=WEBAPP_PORT,
+    )
+# if __name__ == '__main__':
+#     from aiogram import executor
+#     executor.start_polling(dp, skip_updates=True)
